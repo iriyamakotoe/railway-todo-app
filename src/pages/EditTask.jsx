ac3e6b808445c0261ff7d5aea81308075f6a0c95
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-23 16:27:39 +0900

    01_ライブラリのバージョンアップをしよう

--- src/pages/EditTask.jsx
+++ src/pages/EditTask.jsx
@@ -3,11 +3,11 @@ import { Header } from '../components/Header'
 import axios from 'axios'
 import { useCookies } from 'react-cookie'
 import { url } from '../const'
-import { useHistory, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import './editTask.scss'
 
 export const EditTask = () => {
-  const history = useHistory()
+  const navigate = useNavigate()
   const { listId, taskId } = useParams()
   const [cookies] = useCookies()
   const [title, setTitle] = useState('')
@@ -36,7 +36,7 @@ export const EditTask = () => {
         },
       })
       .then((res) => {
-        history.push('/')
+        navigate('/')
       })
       .catch((err) => {
         setErrorMessage(`更新に失敗しました。${err}`)
@@ -51,7 +51,7 @@ export const EditTask = () => {
         },
       })
       .then(() => {
-        history.push('/')
+        navigate('/')
       })
       .catch((err) => {
         setErrorMessage(`削除に失敗しました。${err}`)
