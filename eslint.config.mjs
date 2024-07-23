22aa0790f87d269cf00c17c7541614b0b32e0dd5
iriyamakotoe <iriyamakotoe@icloud.com>
2024-07-24 00:02:24 +0900

    02_ESLint, Prettierを導入しよう

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,24 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"],
     settings: {
       react: {
         version: "detect",
       }
-    }
+    },
   },
-  {languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }}},
+  {languageOptions: { globals: {
+    ...globals.browser,
+    ...globals.node
+  } }},
   pluginJs.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+        'react/prop-types': 'off',
+    },
+  }
 ];
